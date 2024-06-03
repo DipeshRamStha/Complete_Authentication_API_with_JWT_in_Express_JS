eb5e290cce547cfc0e48de91aed450f6649c5001
DipeshRamStha <visit.dipesh933@gmail.com>
2024-06-02 23:56:21 -0500

    Created the Database URL and connected by importing the connectDB from the config folder

--- app.js
+++ app.js
@@ -2,13 +2,20 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import cors from "cors";
+import connectDb from "./config/connectdb.js";
 
 const app = express();
 const port = process.env.PORT;
+const DATABASE_URL = process.env.DATABASE_URL;
 
 // CORS Policy
 app.use(cors());
 
+// Database Connection
+connectDb(DATABASE_URL);
+
+// JSON
+app.use(express.json());
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
