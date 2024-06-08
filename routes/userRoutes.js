f6ff8298d3bbf81f6e2b37dd1917bced159a446e
DipeshRamStha <visit.dipesh933@gmail.com>
2024-06-08 15:30:54 -0500

    Generated newHashPassword and updated only the password field using the  operator in the specified user ID and debugged the error such as startsWith select

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,10 @@
 import express from "express";
 const router = express.Router();
 import UserController from "../controllers/userController.js";
+import checkUserAuth from "../middlewares/auth-middleware.js";
+
+// Route Level Middleware - To Protect Route
+router.use("/changepassword", checkUserAuth);
 
 // Public Routes
 router.post("/register", UserController.userRegistration);
